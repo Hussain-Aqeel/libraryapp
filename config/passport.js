e92291d06cf65d74c8505df0e6aa53396d8f5eca
hussain <hussain@ljassim.tech>
2021-12-15 21:08:45 +0300

    auth finished and edit credintials

--- config/passport.js
+++ config/passport.js
@@ -9,30 +9,6 @@ const System = require('../models/System');
 
 module.exports = function (passport) 
 {
-  passport.use(
-    new LocalStrategy( { usernameField: 'email' }, (email, password, done) => {
-      // Match User
-      User.findOne({ email: email })
-        .then(user => {
-          if(!user) {
-            return done(null, false, { message: 'That email is not registered' })
-          }
-
-          // Match password
-          bcrypt.compare(password, user.password, (err, isMatch) => {
-            if(err) throw err;
-
-            if(isMatch) {
-              return done(null, user);
-            } else {
-              return done(null, false, { message: 'Password incorrect' })
-            }
-          });
-        })
-        .catch(err => console.log(err))
-    })
-  );
-
   passport.use('member-local',
     new LocalStrategy({ usernameField: 'id' }, (id, password, done) => {
       // Match User
