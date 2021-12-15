a315cd1281f540c9a781958c018d269fc4bc3b3f
hussain <hussain@ljassim.tech>
2021-12-15 20:50:47 +0300

    auth finished and edit credintials

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MongoURI: 'mongodb+srv://hussainaqeel:Vh01OZ^i3dy3#wcziF3rxTPAB%agsz@hussaincluster.02ejs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  MongoURI: 'mongodb+srv://hussainaqeel:Vh01OZ^i3dy3#wcziF3rxTPAB%agsz@hussaincluster.02ejs.mongodb.net/library?retryWrites=true&w=majority'
 }
